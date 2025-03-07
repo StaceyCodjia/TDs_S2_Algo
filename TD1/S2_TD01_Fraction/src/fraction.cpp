ca54691d2ccc29c382dc1bca070b25ee9f20cf8d
Stacey <158062961+StaceyCodjia@users.noreply.github.com>
2025-03-07 19:13:44 +0100

    Add files via upload

---
+++ TD1/S2_TD01_Fraction/src/fraction.cpp
@@ -0,0 +1,104 @@
+#include "fraction.hpp"
+#include "utils.hpp"
+
+#include <iostream>
+//* Exo 2
+void Fraction::display() {
+    std::cout << numerator << "/" << denominator; 
+}
+std::ostream& operator<<(std::ostream& os, Fraction const& f){
+    os <<  f.numerator << "/" << f.denominator;
+    return os;
+}
+
+//* Exo 1
+// Fraction operator+(Fraction const& f1, Fraction const& f2) {
+//     return simplify({
+//         f1.numerator * f2.denominator + f2.numerator * f1.denominator,
+//         f1.denominator * f2.denominator
+//     });
+// }
+
+Fraction operator+(Fraction f1, Fraction const& f2){
+    f1 += f2;
+    return f1;
+}
+// Fraction operator-(Fraction const& f1, Fraction const& f2) {
+//     return simplify({
+//         f1.numerator * f2.denominator - f2.numerator * f1.denominator,
+//         f1.denominator * f2.denominator
+//     });
+// }
+
+Fraction operator-(Fraction f1, Fraction const& f2){
+    f1 -= f2;
+    return f1;
+}
+// Fraction operator*(Fraction const& f1, Fraction const& f2) {
+//     return simplify({
+//         f1.numerator * f2.numerator,
+//         f1.denominator * f2.denominator
+//     });
+// }
+
+Fraction operator*(Fraction f1, Fraction const& f2){
+    f1 *= f2;
+    return f1;
+}
+// Fraction operator/(Fraction const& f1, Fraction const& f2) {
+//     return simplify({
+//         f1.numerator * f2.denominator,
+//         f1.denominator * f2.numerator
+//     });
+// }
+
+Fraction operator/(Fraction f1, Fraction const& f2){
+    f1 /= f2;
+    return f1;
+}
+
+//* Exo 3
+bool operator==(Fraction const& f1, Fraction const& f2) {
+    return f1.numerator == f2.numerator && f1.denominator == f2.denominator;
+}
+bool operator!=(Fraction const& f1, Fraction const& f2) {
+    return !(f1 == f2);
+}
+
+//* Exo 4
+bool operator< (Fraction const& f1, Fraction const& f2) {
+    return f1.numerator * f2.denominator < f2.numerator * f1.denominator;      
+}
+bool operator <= (Fraction const& f1, Fraction const& f2){
+    return f1 < f2 || f1 == f2;
+}
+bool operator > (Fraction const& f1, Fraction const& f2){
+    return !(f1 <= f2);
+}
+bool operator >= (Fraction const& f1, Fraction const& f2){
+    return !(f1 < f2);
+}
+
+//* Exo 5
+void Fraction ::operator += (Fraction const& f){
+    numerator = numerator*f.denominator + f.numerator*denominator;
+    denominator = denominator* f.denominator;
+    *this = simplify(*this);
+}
+void Fraction ::operator-=(Fraction const& f) {
+       numerator = numerator * f.denominator - f.numerator * denominator,
+       denominator = denominator* f.denominator;
+       *this = simplify(*this);
+}
+void Fraction ::operator*=(Fraction const& f) {
+    numerator = numerator * f.numerator,
+    denominator = denominator* f.denominator;
+    *this = simplify(*this);
+}
+void Fraction ::operator/=(Fraction const& f) {
+    numerator = numerator * f.denominator,
+    denominator = denominator* f.denominator;
+    *this = simplify(*this);
+}
+
+//* Exo 6
---
+++ TD1/S2_TD01_Fraction/src/fraction.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+struct Fraction {
+    int numerator { 0 };
+    int denominator { 1 };
+
+    void display();
+    void operator+=(Fraction const& f);
+    void operator-=(Fraction const& f);
+    void operator*=(Fraction const& f);
+    void operator/=(Fraction const& f);
+};
+
+Fraction operator+(Fraction const& f1, Fraction const& f2);
+Fraction operator-(Fraction const& f1, Fraction const& f2);
+Fraction operator*(Fraction const& f1, Fraction const& f2);
+Fraction operator/(Fraction const& f1, Fraction const& f2);
+bool operator==(Fraction const& f1, Fraction const& f2);
+bool operator!=(Fraction const& f1, Fraction const& f2);
