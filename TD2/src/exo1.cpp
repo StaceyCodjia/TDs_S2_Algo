1051b4a8987e5659822acd88b513809ca82b5de9
Stacey <staceycodjia@gmail.com>
2025-03-12 12:14:55 +0100

    Exo 1 TD2

--- TD2/src/exo1.cpp
+++ TD2/src/exo1.cpp
@@ -14,9 +14,61 @@ std::vector<std::string> split_string(std::string const& s)
     return std::vector<std::string>(std::istream_iterator<std::string>(in), 
     std::istream_iterator<std::string>()); 
 }
+
+bool is_floating(std::string const& s){
+    for (char c : s){
+        if (!std::isdigit(c) && c != '.'){
+            return false;
+        }
+    }
+    return true;
+}
+
+float npi_evaluate(std::vector<std::string> const& tokens){
+    std::stack<float> stack;
+    for (std::string token : tokens){
+        if (is_floating(token)){
+            stack.push(std::stof(token));
+        } else {
+            float operand2 = stack.top();
+            stack.pop();
+            float operand1 = stack.top();
+            stack.pop();
+            if (token == "+"){
+                stack.push(operand1 + operand2);
+            } else if (token == "-"){
+                stack.push(operand1 - operand2);
+            } else if (token == "*"){
+                stack.push(operand1 * operand2);
+            } else if (token == "/"){
+                stack.push(operand1 / operand2);
+            }else if (token == "^"){
+                stack.push(std::pow(operand1, operand2));
+            } else {
+                throw std::runtime_error("Operateur invalide : " + token);
+        }
+    }
+}
+return stack.top();
+throw std::runtime_error("Pas de token");
+return 0.f;
+}
+
 int main(){
 
     std::string npi;
     std::cout << "Entrez une expression en notation polonaise inversee : ";
     std::getline(std::cin, npi); 
+    std::vector<std::string> tokens = split_string(npi);
+
+    for (std::string token : tokens){
+        if (is_floating(token)){
+            std::cout << "Nombre : " << token << std::endl;
+        } else {
+            std::cout << "Operateur : " << token << std::endl;
+        }
+    }
+
+    std::cout << "Resultat : " << npi_evaluate(tokens) << std::endl;
+    return 0;
 }
