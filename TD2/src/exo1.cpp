7108aa8c453b0f907726237f873353be6982377a
Stacey <staceycodjia@gmail.com>
2025-03-12 11:48:57 +0100

    Q.1 TD2

--- TD2/src/exo1.cpp
+++ TD2/src/exo1.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <cctype>
+#include <cmath>
 
+std::vector<std::string> split_string(std::string const& s)
+{
+    std::istringstream in(s); // transforme une chaîne en flux de caractères, cela simule un flux comme l'est std::cin
+    // l’itérateur va lire chaque element de "in", comme un flux d'entrée, chaque élément est séparé par un espace
+    return std::vector<std::string>(std::istream_iterator<std::string>(in), 
+    std::istream_iterator<std::string>()); 
+}
 int main(){
-    
+
+    std::string npi;
+    std::cout << "Entrez une expression en notation polonaise inversee : ";
+    std::getline(std::cin, npi); 
 }
