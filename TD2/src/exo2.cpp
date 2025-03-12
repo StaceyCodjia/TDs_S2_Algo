38ec689b944dae0325c4d20a4d8d784c846e6f54
Stacey <staceycodjia@gmail.com>
2025-03-12 12:34:15 +0100

    Exo2 Q.1

---
+++ TD2/src/exo2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+enum class Operator { ADD, SUB, MUL, DIV};
+enum class TokenType { NUMBER, OPERATOR };
+struct Token {
+  TokenType type;
+  float value;
+  Operator op;
+};
+Token make_token(float value) {
+    return Token{TokenType::NUMBER, value, Operator{}};
+}
+Token make_token(Operator op) {
+    return Token{TokenType::OPERATOR, 0.0f, op};
+}
+
+
+int main(){
+
+}
