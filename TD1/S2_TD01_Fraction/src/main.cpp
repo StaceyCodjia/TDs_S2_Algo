801312f74ee0d905f708ad7f56f0c0f21c887e5b
Stacey <staceycodjia@gmail.com>
2025-03-18 09:01:21 +0100

    Finitions TD1

--- TD1/S2_TD01_Fraction/src/main.cpp
+++ TD1/S2_TD01_Fraction/src/main.cpp
@@ -5,13 +5,11 @@ int main()
 {
     Fraction f1 { 3, 4 };
     Fraction f2 { 1, 2 };
-
+//Exo 1 - 2
     std::cout << "f1 = "<< f1 << std::endl;
     std::cout << " f2 = "<< f2 << std::endl;
 
-// Exercice 1-2
-    std::cout << "Exercice 1-2 \n"; {
-
+ {
     Fraction f3 = f1 + f2;
     std::cout << "f1 + f2 = " << f3 << std::endl;
 
@@ -24,53 +22,47 @@ int main()
     f3 = f1 / f2;
     std::cout << "f1 / f2 = " << f3 << std::endl;
 }
-
-// Exercice 3
-    std::cout << "Exercice 3 \n";
-
+ //Exo 3
     if (f1 == f2) {
-        std::cout << "f1 est égal à f2" << std::endl;
+        std::cout << "f1 egal a f2" << std::endl;
     } else {
-        std::cout << "f1 n'est pas égal à f2" << std::endl;
+        std::cout << "f1 n'est pas egal a f2" << std::endl;
     }
 
     if (f1 != f2) {
-        std::cout << "f1 est différent de f2" << std::endl;
+        std::cout << "f1 est different de f2" << std::endl;
     } else {
         std::cout << "f1 = f2" << std::endl;
     }
 
 
-// Exercice 4
-    std::cout << "Exercice 4 \n";
+// Exo 4
 
     if (f1 < f2) {
-        std::cout << "f1 inférieur à f2" << std::endl;
+        std::cout << "f1 < f2" << std::endl;
     } else {
-        std::cout << "f1 pas inférieur à f2" << std::endl;
+        std::cout << "f1 pas inferieur a f2" << std::endl;
     }
 
     if (f1 <= f2) {
-        std::cout << "f1 inférieur ou égal à f2" << std::endl;
+        std::cout << "f1 <=  f2" << std::endl;
     } else {
-        std::cout << "f1 supérieur à f2" << std::endl;
+        std::cout << "f1 > f2" << std::endl;
     }
 
     if (f1 > f2) {
-        std::cout << "f1 supérieur à f2" << std::endl;
+        std::cout << "f1 > f2" << std::endl;
     } else {
-        std::cout << "f1 pas supérieur à f2" << std::endl;
+        std::cout << "f1 pas superieur a f2 " << std::endl;
     }
 
     if (f1 >= f2) {
-        std::cout << "f1 supérieur ou égal à f2" << std::endl;
+        std::cout << "f1 >= f2 " << std::endl;
     } else {
-        std::cout << "f1 inférieur à f2" << std::endl;
+        std::cout << "f1 < f2 " << std::endl;
     }
 
-// 5
-
-  std::cout << "Exercice 5\n";
+// Exo 5
     
     Fraction f3 = f1;
     f3 += f2;
@@ -89,8 +81,7 @@ int main()
     std::cout << "f1 /= f2 = " << f3 << std::endl;
 
 
-    // Exercice 6
-    std::cout << "Exercice 6\n";
+    // Exo 6
     float floatNumber = static_cast<float>(f1);
     std::cout << "f1 en format de type float = " << floatNumber << std::endl;
         return 0;
