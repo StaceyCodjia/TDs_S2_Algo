9dccc499cf83cb00e6d829e7b45188a1dc1df719
Stacey <staceycodjia@gmail.com>
2025-03-19 10:57:43 +0100

    Création TD3

---
+++ TD3/src/exo1.cpp
@@ -0,0 +1,8 @@
+#include "fonctions.hpp"
+
+
+
+int main(){
+
+   
+}
---
+++ TD3/src/fonctions.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+
+// Exo 1
